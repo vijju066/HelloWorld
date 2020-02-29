d1ab7d54c07533be2f68cc4b2d559c4bda32d4ff
vijju066 <vijaykumar066@gmail.com>
2020-02-29 12:27:54 +0530

    commited hello world

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package Vijay;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1; i<=10;i++) {
+			System.out.println("Hello World==>"+i);
+		}
+	}
+
+}
